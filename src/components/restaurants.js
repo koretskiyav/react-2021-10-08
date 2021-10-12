ce883b5a8cd62dac1323f6cfab4f87db653c72ae
koretskiyav <koretskiyav@gmail.com>
2021-10-12 20:38:14 +0300

    add useState, useMemo hooks

--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -1,9 +1,24 @@
+import { useState, useMemo } from 'react';
 import Menu from './menu';
+import Tabs from './tabs';
 
 export default function Restaurants({ restaurants }) {
+  const [activeId, setActiveId] = useState(restaurants[0].id);
+
+  const tabs = useMemo(
+    () => restaurants.map(({ id, name }) => ({ id, label: name })),
+    [restaurants]
+  );
+
+  const activeRestaurant = useMemo(
+    () => restaurants.find((restaurant) => restaurant.id === activeId),
+    [activeId, restaurants]
+  );
+
   return (
     <div>
-      <Menu menu={restaurants[0].menu} />
+      <Tabs tabs={tabs} onChange={setActiveId} />
+      <Menu menu={activeRestaurant.menu} />
     </div>
   );
 }
