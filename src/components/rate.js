6ac139fd445ac6042d80a692c4fb8efc893feda5
alexdolg1967 <alexdolg1967@mail.ru>
2021-10-13 11:09:32 +0300

    rate 2

--- src/components/rate.js
+++ src/components/rate.js
@@ -3,15 +3,17 @@ import styles from './rate.module.css';
 import { ReactComponent as Plus } from '../icons/plus.svg';
 
 function Rate({ value }) {
-  const emptyRate = 5 - value;
-  let str = [];
-  for (let i = 0; i < value; i++) {
-    str.push(`<span>
-        <Plus className={styles.icon} />
-      </span>`);
-  }
-  console.log(str);
-  return <div className={styles.rate}>{str.map((element) => str.values)}</div>;
+  let rates = [1, 2, 3, 4, 5];
+
+  return (
+    <div className={styles.rate}>
+      {rates.map((element) => (
+        <span>
+          <Plus className={styles.icon} />
+        </span>
+      ))}
+    </div>
+  );
 }
 
 export default Rate;
