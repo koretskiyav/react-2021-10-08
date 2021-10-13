5c6aa347523ed38a22f536e6f94c3d2b7cbfaba7
alexdolg1967 <alexdolg1967@mail.ru>
2021-10-13 19:55:40 +0300

    add styles menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,11 +1,15 @@
 import Product from './product';
+import styles from './menu.module.css';
 
 export default function Menu({ menu }) {
   return (
-    <div>
-      {menu.map((product) => (
-        <Product key={product.id} product={product} />
-      ))}
+    <div className={styles.wrapper}>
+      <h3>Menu</h3>
+      <div className={styles.product__list}>
+        {menu.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 }
