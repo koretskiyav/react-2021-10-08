77ba5bdbf7404e951a0c5802e3014e07dfa57447
Bogdan1988 <bogdan.tur1988@gmail.com>
2021-10-13 10:45:17 +0300

    1.3 Added Restaurant component and other small fixes

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,8 +1,10 @@
 import Product from './product';
+import React from "react";
 
 export default function Menu({ menu }) {
   return (
     <div>
+      <h3>Menu</h3>
       {menu.map((product) => (
         <Product key={product.id} product={product} />
       ))}
