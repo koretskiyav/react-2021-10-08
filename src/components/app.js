afc57263d0afce1f8ca7fbeed9e33b09ba345408
alexdolg1967 <alexdolg1967@mail.ru>
2021-10-13 19:54:49 +0300

    add container

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import { PureComponent } from 'react';
 import Restaurants from './restaurants';
+import styles from './app.module.css';
 
 export default class App extends PureComponent {
   render() {
     return (
-      <div>
+      <div className={styles.container}>
         <Restaurants restaurants={this.props.restaurants} />
       </div>
     );
