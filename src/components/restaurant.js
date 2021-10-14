c320a71dc39c38ab877b7eb4a648441e5f324fdf
Seyfedin Navruzov <lfnickname@gmail.com>
2021-10-14 21:16:04 +0300

    commit

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,10 +1,21 @@
 import Reviews from './reviews';
 import Menu from './menu';
+import { restaurants } from '../fixtures';
+import Rate from './rate';
 
 export default function Restaurant(props) {
+  const filtredResult = restaurants.filter((elem) => elem.id === props.id);
+  const rvs = filtredResult[0].reviews;
+  let sum = 0;
+  for (let element of rvs) {
+    sum += element.rating;
+  }
+  const result = sum / rvs.length;
   return (
     <div>
       <Reviews restId={props.id} />
+      avg score:
+      <Rate value={result} />
       <Menu menu={props.menu} />
     </div>
   );
