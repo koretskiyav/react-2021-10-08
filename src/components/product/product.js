a3035a165ec5d2804a730bcc3ac502bc4d560ffd
koretskiyav <koretskiyav@gmail.com>
2021-10-15 21:31:09 +0300

    add product tests

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -3,10 +3,15 @@ import PropTypes from 'prop-types';
 import counter from '../../hocs/counter';
 import styles from './product.module.css';
 import Button from '../button';
+import { useEffect } from 'react';
 
-function Product({ product, amount, decrement, increment }) {
+function Product({ product, amount, decrement, increment, fetchData }) {
+  useEffect(() => {
+    fetchData && fetchData(product.id);
+  }, []); // eslint-disable-line
+
   return (
-    <div className={styles.product}>
+    <div className={styles.product} data-id="product">
       <div className={styles.content}>
         <div>
           <h4 className={styles.title}>{product.name}</h4>
@@ -15,10 +20,16 @@ function Product({ product, amount, decrement, increment }) {
         </div>
         <div>
           <div className={styles.counter}>
-            <div className={styles.count}>{amount}</div>
+            <div className={styles.count} data-id="product-amount">
+              {amount}
+            </div>
             <div className={styles.buttons}>
               <Button onClick={decrement} icon="minus" />
-              <Button onClick={increment} icon="plus" />
+              <Button
+                onClick={increment}
+                data-id="product-increment"
+                icon="plus"
+              />
             </div>
           </div>
         </div>
