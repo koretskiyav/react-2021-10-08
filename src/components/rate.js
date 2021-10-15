011ddd6f7f2c83ddd35f02774f060a9e3c4a0b1d
Iryna Kashyntseva <iryna.kashyntseva@grandparade.co.uk>
2021-10-15 13:04:24 +0200

    Fix after review

--- src/components/rate.js
+++ src/components/rate.js
@@ -1,19 +1,18 @@
 import { useMemo } from 'react';
 import { ReactComponent as Star } from '../icons/rating.svg';
+import { MAX_POSSIBLE_RATING } from '../constants';
 import styles from './restaurant.module.css';
 
 const Rate = ({ value, className }) => {
-  const rating = useMemo(() => {
-    const stars = [];
+  const rating = useMemo(
+    () =>
+      Array.from({ length: MAX_POSSIBLE_RATING }, (_, i) => {
+        const selected = i < value ? styles.selected : '';
 
-    for (let i = 0; i < 5; i++) {
-      const selected = i < value ? styles.selected : '';
-
-      stars.push(<Star key={i} className={`${styles.icon} ${selected}`} />);
-    }
-
-    return stars;
-  }, [value]);
+        return <Star key={i} className={`${styles.icon} ${selected}`} />;
+      }),
+    [value]
+  );
 
   return <div className={className}>{rating}</div>;
 };
