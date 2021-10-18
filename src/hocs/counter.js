1353f290688d91314e9a66eaae7ead50fa46847a
Igor Novak <igor.n@miro.com>
2021-10-18 05:52:05 +0200

    Add decrement test

--- src/hocs/counter.js
+++ src/hocs/counter.js
@@ -1,6 +1,6 @@
 import useAmount from '../hooks/use-amount';
 
 export default (WrappedComponent) => (props) => {
-  const amountProps = useAmount(0);
+  const amountProps = useAmount(props.amount || 0);
   return <WrappedComponent {...props} {...amountProps} />;
 };
