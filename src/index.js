7e2b1be84c3dccb86e3a23d33510a01bea3b6015
koretskiyav <koretskiyav@gmail.com>
2021-10-19 20:29:46 +0300

    add simple reducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import { restaurants } from './fixtures';
+import store from './redux/store';
 import './index.css';
+
+// DEV ONLY!!!
+window.store = store;
 
 ReactDOM.render(
   <App restaurants={restaurants} />,
---
+++ src/redux/reducer.js
@@ -0,0 +1,10 @@
+export default function (state = 0, action) {
+  switch (action.type) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    default:
+      return state;
+  }
+}
