158f5451edb104874dd1b49dc9d6673e6e4bb48c
koretskiyav <koretskiyav@gmail.com>
2021-10-22 18:34:52 +0300

    add ReviewForm

--- src/components/rate/rate.js
+++ src/components/rate/rate.js
@@ -5,13 +5,17 @@ import { ReactComponent as Star } from '../../icons/star.svg';
 
 import styles from './rate.module.css';
 
-const Rate = ({ value }) => (
+const Rate = ({ value, onChange }) => (
   <div>
     {[...Array(5)].map((_, i) => (
       <Star
         key={i}
         data-id={i <= value - 1 ? 'full-star' : 'empty-star'}
-        className={cn(styles.star, { [styles.checked]: i <= value - 1 })}
+        className={cn(styles.star, {
+          [styles.checked]: i <= value - 1,
+          [styles.clickable]: !!onChange,
+        })}
+        onClick={onChange ? () => onChange(i + 1) : undefined}
       />
     ))}
   </div>
