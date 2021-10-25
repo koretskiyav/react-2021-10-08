95c948c006bca52cf528cb6584f7c8d02ca0a6bb
Seyfedin Navruzov <lfnickname@gmail.com>
2021-10-25 14:39:46 +0300

    add Restaurant reducer, User reducer, fix Review

--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -2,12 +2,13 @@ import PropTypes from 'prop-types';
 import Review from './review';
 import ReviewForm from './review-form';
 import styles from './reviews.module.css';
+import { connect } from 'react-redux';
 
-const Reviews = ({ reviews }) => {
+const Reviews = ({ reviewids, reviews }) => {
   return (
     <div className={styles.reviews}>
-      {reviews.map((review) => (
-        <Review key={review.id} {...review} />
+      {reviewids.map((review) => (
+        <Review key={review} rw={reviews[review]} />
       ))}
       <ReviewForm />
     </div>
@@ -15,11 +16,15 @@ const Reviews = ({ reviews }) => {
 };
 
 Reviews.propTypes = {
-  reviews: PropTypes.arrayOf(
+  reviewids: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
     }).isRequired
   ).isRequired,
 };
 
-export default Reviews;
+const mapStateToProps = (state) => ({
+  reviews: state.reviews,
+});
+
+export default connect(mapStateToProps)(Reviews);
--- src/redux/reducer/reviews.js
+++ src/redux/reducer/reviews.js
@@ -1,4 +1,9 @@
-import { normalizedReviews as defaultReviews } from '../../fixtures';
+import { normalizedReviews } from '../../fixtures';
+
+const defaultReviews = normalizedReviews.reduce(
+  (acc, reviews) => ({ ...acc, [reviews.id]: reviews }),
+  {}
+);
 
 export default (reviews = defaultReviews, action) => {
   const { type } = action;
