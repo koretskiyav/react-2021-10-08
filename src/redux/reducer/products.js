feb67059b3243aa6766ad9fe25dae7cd0d989ebc
alexdolg1967 <alexdolg1967@mail.ru>
2021-10-27 13:58:04 +0300

    add immer in reducer

--- src/redux/reducer/products.js
+++ src/redux/reducer/products.js
@@ -1,21 +1,37 @@
-import { normalizedProducts } from '../../fixtures';
 import produce from 'immer';
-import { FAILURE, LOAD_PRODUCTS, REQUEST, SUCCESS } from '../constants';
 import { arrToMap } from '../utils';
+import { LOAD_PRODUCTS, REQUEST, SUCCESS, FAILURE } from '../constants';
 
 const initialState = {
-  activeId: null,
-  loading: false,
-  loaded: false,
   entities: {},
+  loading: {},
+  loaded: {},
   error: null,
 };
 
-export default produce((draft = arrToMap(normalizedProducts), action) => {
-  const { type } = action;
+export default (state = initialState, action) =>
+  produce(state, (draft) => {
+    const { type, restId, data, error } = action;
 
-  switch (type) {
-    default:
-      return draft;
-  }
-});
+    switch (type) {
+      case LOAD_PRODUCTS + REQUEST: {
+        draft.loading[restId] = true;
+        break;
+      }
+      case LOAD_PRODUCTS + SUCCESS: {
+        draft.entities = { ...draft.entities, ...arrToMap(data) };
+        draft.loading[restId] = false;
+        draft.loaded[restId] = true;
+        draft.error = null;
+        break;
+      }
+      case LOAD_PRODUCTS + FAILURE: {
+        draft.loading[restId] = false;
+        draft.loaded[restId] = false;
+        draft.error = error;
+        break;
+      }
+      default:
+        return;
+    }
+  });
