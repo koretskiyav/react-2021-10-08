6852b6db47b075d108c53a49f0fbeb31863d07ab
alexdolg1967 <alexdolg1967@mail.ru>
2021-10-27 16:16:20 +0300

    add immer in users

--- src/redux/reducer/users.js
+++ src/redux/reducer/users.js
@@ -1,12 +1,40 @@
 import produce from 'immer';
-import { ADD_REVIEW } from '../constants';
-import { normalizedUsers } from '../../fixtures';
+import {
+  ADD_REVIEW,
+  LOAD_USERS,
+  REQUEST,
+  FAILURE,
+  SUCCESS,
+} from '../constants';
 import { arrToMap } from '../utils';
 
-export default produce((draft = arrToMap(normalizedUsers), action) => {
-  const { type, review, userId } = action;
+const initialState = {
+  loading: false,
+  loaded: false,
+  entities: {},
+  error: null,
+};
+
+export default produce((draft = initialState, action) => {
+  const { type, review, userId, data, error } = action;
 
   switch (type) {
+    case LOAD_USERS + REQUEST:
+      draft.loading = true;
+      draft.loaded = false;
+      draft.error = null;
+      break;
+
+    case LOAD_USERS + SUCCESS:
+      draft.entities = arrToMap(data);
+      draft.loading = false;
+      draft.loaded = true;
+      break;
+    case LOAD_USERS + FAILURE:
+      draft.loading = false;
+      draft.loaded = false;
+      draft.error = error;
+      break;
     case ADD_REVIEW:
       const { name } = review;
       draft[userId] = { id: userId, name };
