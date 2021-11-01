687f0db7077de6887b645528293449c25df5f200
Olimpieva Natalya <ukurennyi.koshak@gmail.com>
2021-11-02 00:36:18 +0300

    feat: routs, redirect menu/reviews, product links to restaurants

--- src/components/basket/basket-item/basket-item.js
+++ src/components/basket/basket-item/basket-item.js
@@ -1,5 +1,7 @@
 import { connect } from 'react-redux';
 import cn from 'classnames';
+import { Link, } from 'react-router-dom';
+
 import { increment, decrement, remove } from '../../../redux/actions';
 import Button from '../../button';
 import styles from './basket-item.module.css';
@@ -8,6 +10,7 @@ function BasketItem({
   product,
   amount,
   subtotal,
+  restId,
   increment,
   decrement,
   remove,
@@ -15,7 +18,8 @@ function BasketItem({
   return (
     <div className={styles.basketItem}>
       <div className={styles.name}>
-        <span>{product.name}</span>
+        <Link to={`/restaurants/${restId}`}>{product.name}
+        </Link>
       </div>
       <div className={styles.info}>
         <div className={styles.counter}>
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -27,24 +27,39 @@ export const restaurantsListSelector = createSelector(
   restaurantsSelector,
   Object.values
 );
+const productsRestaurantsIdsSelector = createSelector(
+  restaurantsListSelector,
+  (restaurants) => {
+    const productsRestaurantsIds = {};
+    restaurants.map((restaurant) => {
+      return restaurant.menu.map(productId => productsRestaurantsIds[productId] = restaurant.id);
+    })
+    return productsRestaurantsIds;
+  }
+);
 
 export const restaurantSelector = (state, { id }) =>
   restaurantsSelector(state)[id];
 export const productSelector = (state, { id }) => productsSelector(state)[id];
 export const reviewSelector = (state, { id }) => reviewsSelector(state)[id];
 export const amountSelector = (state, { id }) => orderSelector(state)[id] || 0;
+
 export const orderProductsSelector = createSelector(
   orderSelector,
   productsSelector,
-  (order, products) =>
+  productsRestaurantsIdsSelector,
+  (order, products, productsRestaurantsIds) =>
     Object.keys(order)
       .filter((productId) => order[productId] > 0)
       .map((productId) => products[productId])
-      .map((product) => ({
-        product,
-        amount: order[product.id],
-        subtotal: order[product.id] * product.price,
-      }))
+      .map((product) => {
+        return {
+          product,
+          amount: order[product.id],
+          subtotal: order[product.id] * product.price,
+          restaurantId: productsRestaurantsIds[product.id]
+        }
+      })
 );
 
 export const totalSelector = createSelector(
