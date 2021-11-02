6cbcc5c0eaa9a0d03534edf278ad9b024ed853dd
Iryna Kashyntseva <iryna.kashyntseva@grandparade.co.uk>
2021-11-02 14:48:07 +0200

    3. - Links from Basket Item to Restaurant

--- src/components/basket/basket-item/basket-item.js
+++ src/components/basket/basket-item/basket-item.js
@@ -1,10 +1,13 @@
 import { connect } from 'react-redux';
 import cn from 'classnames';
 import { increment, decrement, remove } from '../../../redux/actions';
+import { Link } from 'react-router-dom';
 import Button from '../../button';
 import styles from './basket-item.module.css';
+import { restaurantsListSelector } from '../../../redux/selectors';
 
 function BasketItem({
+  restId,
   product,
   amount,
   subtotal,
@@ -15,7 +18,7 @@ function BasketItem({
   return (
     <div className={styles.basketItem}>
       <div className={styles.name}>
-        <span>{product.name}</span>
+        <Link to={`/restaurants/${restId}`}>{product.name}</Link>
       </div>
       <div className={styles.info}>
         <div className={styles.counter}>
@@ -30,10 +33,16 @@ function BasketItem({
   );
 }
 
+const matchStateToProps = (state, props) => ({
+  restId: restaurantsListSelector(state, props).find(({ menu }) =>
+    menu.includes(props.product.id)
+  ).id,
+});
+
 const mapDispatchToProps = (dispatch, ownProps) => ({
   increment: () => dispatch(increment(ownProps.product.id)),
   decrement: () => dispatch(decrement(ownProps.product.id)),
   remove: () => dispatch(remove(ownProps.product.id)),
 });
 
-export default connect(null, mapDispatchToProps)(BasketItem);
+export default connect(matchStateToProps, mapDispatchToProps)(BasketItem);
